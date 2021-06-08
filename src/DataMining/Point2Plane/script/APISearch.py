aa92874d2c47ab66d54dfd63a144ee0ae3484b3c
Peace4lv <2114768901@qq.com>
2021-06-08 22:30:11 +0800

    高德-poi

--- src/DataMining/Point2Plane/script/APISearch.py
+++ src/DataMining/Point2Plane/script/APISearch.py
@@ -6,19 +6,29 @@ from requests import get
 
 
 class APISearch():
-    UrlHead = "https://restapi.amap.com/v5/place/text?"
-    APIKey = "key=d510b9d4b6de357d6084b22a7fb7b0a2"
-    # key=<用户的key>&location=116.473168,39.993015&radius=10000
+    UrlPart = {
+        "urlhead":
+        "https://restapi.amap.com/v3/place/around?key=6de2b5a95a0e35fc03782b55d455d8c4",
+        "key": "&key=6de2b5a95a0e35fc03782b55d455d8c4",
+        "keywords": "&keywords=学校|学校",
+        "location": "&location=104.01775,30.489013",
+        "radius": "&=5000"
+    }
 
     def __init__(self):
         super().__init__()
         # 验证网站链接是否正确
         if True:
-            TestUrl = self.UrlHead+self.APIKey + \
-                "&keywords=北京大学&types=010101"
+            TestUrl = self.UrlPart["urlhead"] + self.UrlPart[
+                "location"] + self.UrlPart["radius"] + self.UrlPart["keywords"]
         else:
-            TestUrl = "https://restapi.amap.com/v3/place/text?keywords=北京大学&city=beijing&output=xml&offset=20&page=1&key=d510b9d4b6de357d6084b22a7fb7b0a2&extensions=all"
+            TestUrl = "https://restapi.amap.com/v3/place/around?key=6de2b5a95a0e35fc03782b55d455d8c4&location=116.473168,39.993015&radius=10000&types=011100"
         html = get(TestUrl).json()
+        pois = html['pois']
+        for k in pois:
+            print(k["type"] + "\n")
+            # if k["type"].contains('科教文化服务;学校'):
+            #     print(k)
         print(html["info"])
 
     # 搜索周边的学校
