cfeda18353af4ec51883965e6bd367b956aa9722
Risky Jr <2114768901@qq.com>
2021-06-09 15:55:14 +0800

    path

--- src/DataMining/Point2Plane/script/APISearch.py
+++ src/DataMining/Point2Plane/script/APISearch.py
@@ -10,9 +10,9 @@ class APISearch():
         "urlhead":
         "https://restapi.amap.com/v3/place/around?key=6de2b5a95a0e35fc03782b55d455d8c4",
         "key": "&key=6de2b5a95a0e35fc03782b55d455d8c4",
-        "keywords": "&keywords=学校|学校",
-        "location": "&location=104.01775,30.489013",
-        "radius": "&=5000"
+        "keywords": "&keywords=学校",
+        "location": "&location=103.968104,30.672837",
+        "radius": "&=1000"
     }
 
     def __init__(self):
@@ -24,17 +24,22 @@ class APISearch():
         else:
             TestUrl = "https://restapi.amap.com/v3/place/around?key=6de2b5a95a0e35fc03782b55d455d8c4&location=116.473168,39.993015&radius=10000&types=011100"
         html = get(TestUrl).json()
-        pois = html['pois']
-        for k in pois:
-            print(k["type"] + "\n")
-            # if k["type"].contains('科教文化服务;学校'):
-            #     print(k)
-        print(html["info"])
-
+        if not html['info'] == "OK":
+            self.html = html['info']
+            print(html['info'])
+            return
+        else:
+            print("url test succeed")
+            self.html = html
     # 搜索周边的学校
+
     def School(self):
-        pass
+        pois = self.html['pois']
+        for k in pois:
+            print(k['pname'] + k['cityname']+k['adname'] +
+                  k['address'] + "\n"+k['name']+k['location']+"\n")
 
 
 if __name__ == "__main__":
     aps = APISearch()
+    aps.School()
