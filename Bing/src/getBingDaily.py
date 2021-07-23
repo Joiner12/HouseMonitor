07e584ab40a806d0fd75e20e0bb3e491fb8a1f9f
Risky Jr <2114768901@qq.com>
2021-07-23 17:25:25 +0800

    实现bing单个背景下载

--- Bing/src/getBingDaily.py
+++ Bing/src/getBingDaily.py
@@ -16,6 +16,7 @@ from selenium import webdriver
 from os import path
 # import Beautifulsoap
 from bs4 import BeautifulSoup
+from re import findall
 
 
 class getBing():
@@ -31,11 +32,11 @@ class getBing():
             print(warning_1 + "failed")
         if option_get == 1:
             img_direct_urls = self.__getBingWallPaperToday()
+            self.__saveImages(img_direct_urls)
         elif option_get == 2:
             img_direct_urls = self.__getBingWallPaperRecent()
         else:
             pass
-        degu = 1
 
     # 以双下划线"__"开始的函数为私有函数
     # 获取最近五天壁纸
@@ -47,17 +48,26 @@ class getBing():
     # 获取当天壁纸
     def __getBingWallPaperToday(self):
         img_urls = list()
-        html = requests.get(url="https://cn.bing.com/?mkt=zh-CN")
+        html = requests.get(url="https://cn.bing.com/")
         bs_html = BeautifulSoup(html.text,
                                 'html.parser',
                                 from_encoding='utf-8')
-        img_url = bs_html.find(name='div', attrs={"id": "bgImgProgLoad"})
-        img_urls[0] = img_url
+        img_url = bs_html.find(name='link', attrs={"id": "bgLink"})
+        img_url = r"https://cn.bing.com/"+img_url.get('href')
+        img_urls.append(img_url)
         return img_urls
 
     # 保存图片
-    def __saveImages(self):
-        pass
+    def __saveImages(self, img_urls):
+        # 从链接中提取图片名字
+        # https://cn.bing.com/th?id=OHR.TokyoMetropolis_ZH-CN2580870845_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp
+        if len(img_urls) == 0:
+            return
+        for img_k in img_urls:
+            name = findall('.*OHR\.(.*)_ZH-CN.*', img_k)
+            with open(self.tar_pic_folder+"\\"+name[0]+".png", 'wb') as f:
+                f.write(requests.get(img_k).content)
+            print("save figure %s" % (name))
 
 
 def return_func(x):
@@ -68,5 +78,4 @@ def return_func(x):
 
 if __name__ == "__main__":
     gb = getBing(tar_pic_folder=r"D:\Code\HouseMonitor\Bing\src\bings")
-    # b = return_func(1)
     # debug_line = 1
