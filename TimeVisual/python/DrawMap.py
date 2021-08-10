5af55f945193ffff2cccfaf792b3f1f4df2c53cb
Peace4lv <2114768901@qq.com>
2021-08-10 20:21:26 +0800

    tu

--- TimeVisual/python/DrawMap.py
+++ TimeVisual/python/DrawMap.py
@@ -5,28 +5,23 @@ from pyecharts.globals import ChartType, SymbolType
 
 
 def DrawMap():
-    c = (
-        Geo(init_opts=opts.InitOpts(page_title="Map-CDC"))
-        .add_schema(maptype="成都")
-        .add(
+    c = (Geo(init_opts=opts.InitOpts(page_title="Map-CDC")).add_schema(
+        maptype="china").add(
             "",
-            [("青羊区", 55), ("成华区", 66), ("温江区", 77), ("武侯区", 88)],
+            [("北京", 55), ("上海", 66), ("长沙", 77), ("武汉", 88)],
             type_=ChartType.EFFECT_SCATTER,
             color="white",
-        )
-        .add(
+        ).add(
             "geo",
-            [("青羊区", "成华区"), ("青羊区", "温江区"), ("青羊区", "武侯区"), ("成华区", "温江区")],
+            [("成都", "上海"), ("成都", "拉萨"), ("成都", "武汉"), ("上海", "拉萨")],
             type_=ChartType.LINES,
-            effect_opts=opts.EffectOpts(
-                symbol=SymbolType.ARROW, symbol_size=2, color="blue"
-            ),
+            effect_opts=opts.EffectOpts(symbol=SymbolType.ARROW,
+                                        symbol_size=2,
+                                        color="blue"),
             linestyle_opts=opts.LineStyleOpts(curve=0.2),
-        )
-        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-        .set_global_opts(title_opts=opts.TitleOpts(title="CDC"))
-        .render("..//html//geoTest.html")
-    )
+        ).set_series_opts(label_opts=opts.LabelOpts(
+            is_show=False)).set_global_opts(title_opts=opts.TitleOpts(
+                title="CDC")).render("..//html//geoTest.html"))
     return c
 
 
